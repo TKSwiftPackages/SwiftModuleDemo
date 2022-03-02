a2fea887b7804f850db4e8cc96750229b52fdf65
ZhengXianda <xianda.zheng@jiamiantech.com>
2022-03-02 18:09:59 +0800

    优化Demo

--- SwiftModuleDemo/SwiftModuleDemo/ContentView.swift
+++ SwiftModuleDemo/SwiftModuleDemo/ContentView.swift
@@ -8,22 +8,61 @@
 import SwiftUI
 import Speaker
 import Dog
+import Basket
+import BasketFramework
+import BasketLibrary
+
+extension View {
+    public func space(_ width: CGFloat? = nil, _ color: Color = Color.blue) -> some View {
+        return self.frame(width: width, height: 50, alignment: .center)
+            .cornerRadius(20)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20, style: .continuous)
+                    .stroke(color, lineWidth: 1)
+            )
+    }
+}
 
 struct ContentView: View {
     @State private var msg: String = ""
     
     var body: some View {
         VStack {
-            Text(msg)
-                .frame(width: 200, height: 50, alignment: .center)
-                .cornerRadius(20)
-                .overlay(RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(Color.gray, lineWidth: 1))
-            Button("Say!"){
-                msg = Dog.intro()
+            Text(msg).space(300, Color.gray)
+            Button(action: {
+                msg = Speaker.intro()
+            }){
+                Text("Say!").space(300)
             }
-            .frame(width: 200, height: 50, alignment: .center)
-            .cornerRadius(20)
-            .overlay(RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(Color.blue, lineWidth: 1))
+            Button(action: {
+                msg = Dog.intro()
+            }){
+                Text("Woof!").space(300)
+            }
+            HStack {
+                Button(action: {
+                    msg = Basket.introFramework()
+                }) {
+                    Text("Bang!Bang!Fx!").space(150)
+                }
+                Button(action: {
+                    msg = Basket.introLibrary()
+                }){
+                    Text("Bang!Bang!Lib!").space(150)
+                }
+            }
+            HStack {
+                Button(action: {
+                    msg = BasketFrameworkIntro.intro()
+                }) {
+                    Text("Bang!Fx!").space(150)
+                }
+                Button(action: {
+                    msg = BasketLibraryIntro.intro()
+                }) {
+                    Text("Bang!Fx!").space(150)
+                }
+            }
         }
     }
 }
