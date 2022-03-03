aec42e685c30c795f1e11d02dad2472f641452e9
ZhengXianda <xianda.zheng@jiamiantech.com>
2022-03-03 14:22:04 +0800

    在git库中添加 xc framework

---
+++ Basket/BasketFramework/BasketFramework.xcframework/ios-arm64_x86_64-simulator/BasketFramework.framework/Headers/BasketFramework.h
@@ -0,0 +1,18 @@
+//
+//  BasketFramework.h
+//  BasketFramework
+//
+//  Created by ZhengXianda on 2022/3/2.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BasketFramework.
+FOUNDATION_EXPORT double BasketFrameworkVersionNumber;
+
+//! Project version string for BasketFramework.
+FOUNDATION_EXPORT const unsigned char BasketFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasketFramework/PublicHeader.h>
+
+
